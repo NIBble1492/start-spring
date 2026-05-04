ef8ba3fffa49aedd20c5c78b282914cdda875fd7
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-04 15:11:21 +0900

    feat: Post 엔티티 구조 개선 및 샘플 데이터 저장/조회 로직 구현

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -2,14 +2,25 @@ package com.back.domain.post.post.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
+@ToString
 public class Post {
     @Id // primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private int id; // INT
-    private String title; // varchar(255)
+    private final String title; // varchar(255)
+    @Column(columnDefinition = "TEXT")
+    private final String content;
+
+    public Post() {
+        this.title = "";
+        this.content = "";
+    }
 }
